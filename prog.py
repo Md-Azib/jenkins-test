784cb005124d10d2f31304195feee95af7a6d890
Mohammed Azib <md.azib@SW-LP10504.local>
2021-09-20 02:34:47 +0530

    added python script for ;iquibase query

---
+++ prog.py
@@ -0,0 +1,12 @@
+import os
+
+URL = os.environ['TEST_LIQUIBASE_URL']
+PORT = os.environ['TEST_LIQUIBASE_PORT']
+DATABASE = os.environ['TEST_LIQUIBASE_DATABASE']
+USER_NAME = os.environ['TEST_LIQUIBASE_USER']
+PASSWORD = os.environ['TEST_LIQUIBASE_PASS']
+
+command = "liquibase status --url=jdbc:mysql://" + URL + ":" + PORT + "/" + DATABASE + "--changeLogFile=my_app-wrapper.xml --username=" + USER_NAME + "--password=" + PASSWORD
+print(command)
+os.system(command)
+
